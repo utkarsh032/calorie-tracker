e1608bb2f542a48b8ebb2265f1b3592533b52cf4
utkarsh raj <utkarshraj525@gmail.com>
2025-01-18 02:39:02 +0530

    Developemnt

--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,6 @@ app.use(dishRoutes);
 mongoDB();
 
 
-app.use(express.static(path.join(__dirname, "frontend/build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
-});
-
-
 // Server Connection
 const PORT = 5000;
 app.listen(PORT, () => {
